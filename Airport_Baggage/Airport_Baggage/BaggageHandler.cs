edce39712a4d8690a1e0d35d42c04d2e5bd7da65
Karim Alaa <karimalaa298@gmail.com>
2021-03-23 11:34:54 +0200

    Add implementation for BaggageInfo a class that provide information about arriving flights and the carousels

---
+++ Airport_Baggage/Airport_Baggage/BaggageHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Airport_Baggage
+{
+    public class BaggageHandler : IObservable<BaggageInfo>
+    {
+        public IDisposable Subscribe(IObserver<BaggageInfo> observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Airport_Baggage/Airport_Baggage/BaggageInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Airport_Baggage
+{
+    /// <summary>
+    /// BaggageInfo class provides information about arriving flights and the carousels where baggage from each flight is available for pickup
+    /// </summary>
+    public class BaggageInfo
+    {
+        private int flightNo;
+        private string origin;
+        private int location;
+
+        /// <summary>
+        /// internal constructor
+        /// </summary>
+        /// <param name="flight">represent the flight number</param>
+        /// <param name="from">represent the flight came from any country</param>
+        /// <param name="carousel">represent the flight location</param>
+        internal BaggageInfo(int flight, string from, int carousel)
+        {
+            this.flightNo = flight;
+            this.origin = from;
+            this.location = carousel;
+        }
+
+        /// <summary>
+        /// represent the flight number
+        /// </summary>
+        public int FlightNumber { get { return this.flightNo; } }
+
+        /// <summary>
+        /// represent the flight came from any country
+        /// </summary>
+        public string From { get { return this.origin ; } }
+
+        /// <summary>
+        /// represent the flight location
+        /// </summary>
+        public int Carousel { get { return this.location; }  }
+
+    }
+}
